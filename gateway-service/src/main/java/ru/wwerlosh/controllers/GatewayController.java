1f55e0a3b54ff0a2501615402f34bf9ad8d05b07
wwerlosh <igor.shverlo@yandex.ru>
2024-01-31 16:26:04 +0300

    Added shortenUrl and redirect endpoints.

--- gateway-service/src/main/java/ru/wwerlosh/controllers/GatewayController.java
+++ gateway-service/src/main/java/ru/wwerlosh/controllers/GatewayController.java
@@ -1,16 +1,18 @@
 package ru.wwerlosh.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+import ru.wwerlosh.controllers.dto.Response;
+import ru.wwerlosh.controllers.dto.UrlRequest;
 import ru.wwerlosh.dao.request.SignInRequest;
 import ru.wwerlosh.dao.request.SignUpRequest;
-import ru.wwerlosh.dao.response.JwtAuthenticationResponse;
+import ru.wwerlosh.dao.response.JwtAuthenticationDTO;
 import ru.wwerlosh.services.GatewayService;
 
 @RestController
@@ -23,12 +25,23 @@ public class GatewayController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<JwtAuthenticationResponse> signUp(@RequestBody SignUpRequest request) {
+    public ResponseEntity<JwtAuthenticationDTO> signUp(@RequestBody SignUpRequest request) {
         return ResponseEntity.ok(gatewayService.signUp(request));
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<JwtAuthenticationResponse> signIn(@RequestBody SignInRequest request) {
+    public ResponseEntity<JwtAuthenticationDTO> signIn(@RequestBody SignInRequest request) {
         return ResponseEntity.ok(gatewayService.signIn(request));
+    }
+
+    @PostMapping("/shorten_url")
+    public ResponseEntity<Response> shortenUrl(HttpServletRequest servletRequest,
+                                               @RequestBody UrlRequest request) {
+        return ResponseEntity.ok(gatewayService.shortenUrl(servletRequest, request));
+    }
+
+    @GetMapping("/{token}")
+    public RedirectView redirect(@PathVariable("token") String token) {
+        return gatewayService.redirect(token);
     }
 }
