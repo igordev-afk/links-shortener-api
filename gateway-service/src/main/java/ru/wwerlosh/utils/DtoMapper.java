f291990400b0cede84ec01dda397a9016e13fc26
wwerlosh <igor.shverlo@yandex.ru>
2024-01-31 16:36:53 +0300

    attached custom object mapper, added two convert methods

--- gateway-service/src/main/java/ru/wwerlosh/utils/DtoMapper.java
+++ gateway-service/src/main/java/ru/wwerlosh/utils/DtoMapper.java
@@ -2,25 +2,32 @@ package ru.wwerlosh.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import ru.wwerlosh.controllers.dto.Response;
+import ru.wwerlosh.controllers.dto.UrlRequest;
 import ru.wwerlosh.dao.request.SignInRequest;
 import ru.wwerlosh.dao.request.SignUpRequest;
-import ru.wwerlosh.dao.response.JwtAuthenticationResponse;
+import ru.wwerlosh.dao.response.JwtAuthenticationDTO;
 
 @Component
 public class DtoMapper {
 
     private final ObjectMapper objectMapper;
 
-    public DtoMapper(ObjectMapper objectMapper) {
+    private final ObjectMapper urlResponseObjectMapper;
+
+    public DtoMapper(ObjectMapper objectMapper,
+                     @Qualifier("urlResponseObjectMapper") ObjectMapper urlResponseObjectMapper) {
         this.objectMapper = objectMapper;
+        this.urlResponseObjectMapper = urlResponseObjectMapper;
     }
 
     public String convertSignupRequestToJson(SignUpRequest request) {
         try {
             return objectMapper.writeValueAsString(request);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error converting Url to JSON", e);
+            throw new RuntimeException("Error converting SignUpRequest to JSON", e);
         }
     }
 
@@ -28,16 +35,31 @@ public class DtoMapper {
         try {
             return objectMapper.writeValueAsString(request);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error converting Url to JSON", e);
+            throw new RuntimeException("Error converting SignInRequest to JSON", e);
         }
     }
 
-    public JwtAuthenticationResponse convertToJwtObject(String jsonString) {
+    public String convertUrlRequestToJson(UrlRequest request) {
         try {
-            return objectMapper.readValue(jsonString, JwtAuthenticationResponse.class);
+            return objectMapper.writeValueAsString(request);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error converting JSON to Url", e);
+            throw new RuntimeException("Error converting UrlRequest to JSON", e);
         }
     }
 
+    public JwtAuthenticationDTO convertToJwtObject(String jsonString) {
+        try {
+            return objectMapper.readValue(jsonString, JwtAuthenticationDTO.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error converting JSON to JwtObject", e);
+        }
+    }
+
+    public Response convertToResponse(String jsonString) {
+        try {
+            return urlResponseObjectMapper.readerFor(Response.class).readValue(jsonString);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error converting JSON to Response", e);
+        }
+    }
 }
