287cb87017632c062e4b68900d85fcf71fb6b156
wwerlosh <igor.shverlo@yandex.ru>
2024-02-04 19:49:52 +0800

    a little bit refactoring

--- gateway-service/src/main/java/ru/wwerlosh/utils/JwtUtils.java
+++ gateway-service/src/main/java/ru/wwerlosh/utils/JwtUtils.java
@@ -19,7 +19,7 @@ import java.util.Date;
 public class JwtUtils {
 
     @Value("${spring.data.jwt.secret}")
-    private static String SECRET = "8w9U7eupsVcokibCDUjO59xuTkl9XJvtMJnx0O7ysqmlmYROYHm88YASpGOsPKFP";
+    private String SECRET;
 
     public Claims getClaims(String token) {
         return Jwts
