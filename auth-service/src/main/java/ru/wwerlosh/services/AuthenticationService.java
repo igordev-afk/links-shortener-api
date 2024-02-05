37d578c20c850b6a1ee4446587bcb7669d32a226
wwerlosh <igor.shverlo@yandex.ru>
2024-02-05 09:26:17 +0800

    some refactoring

--- auth-service/src/main/java/ru/wwerlosh/services/AuthenticationService.java
+++ auth-service/src/main/java/ru/wwerlosh/services/AuthenticationService.java
@@ -6,6 +6,5 @@ import ru.wwerlosh.dao.response.JwtAuthenticationResponse;
 
 public interface AuthenticationService {
     JwtAuthenticationResponse signUp(SignUpRequest request);
-
     JwtAuthenticationResponse signIn(SignInRequest request);
 }
