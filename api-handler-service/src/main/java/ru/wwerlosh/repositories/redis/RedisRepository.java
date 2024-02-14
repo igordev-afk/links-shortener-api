5999075327d1930d197ca000f46d9a715316b19e
wwerlosh <igor.shverlo@yandex.ru>
2024-02-14 20:09:57 +0300

    some functions changes (now cache mapped as 'token -> long_url')

--- api-handler-service/src/main/java/ru/wwerlosh/repositories/redis/RedisRepository.java
+++ api-handler-service/src/main/java/ru/wwerlosh/repositories/redis/RedisRepository.java
@@ -1,8 +1,39 @@
 package ru.wwerlosh.repositories.redis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 import ru.wwerlosh.entities.UrlMapping;
+import ru.wwerlosh.utils.UrlMappingSerializer;
 
-public interface RedisRepository {
-    UrlMapping findByLongUrl(String longUrl);
-    void save(UrlMapping url);
+@Component
+public class RedisRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisRepository.class);
+
+    private static final String HASH_KEY = "URL";
+    private final HashOperations<String, String, String> hashOperations;
+    private final UrlMappingSerializer objectMapper;
+
+    public RedisRepository(RedisTemplate<String, String> redisTemplate,
+                               UrlMappingSerializer objectMapper) {
+        this.hashOperations = redisTemplate.opsForHash();
+        this.objectMapper = objectMapper;
+    }
+
+    public String findLongUrlByToken(String token) {
+        String longUrl = hashOperations.get(HASH_KEY, token);
+        if (longUrl == null) {
+            logger.debug("No entry found");
+            return null;
+        }
+        return longUrl;
+    }
+
+    public void save(String token, String longUrl) {
+        hashOperations.put(HASH_KEY, token, longUrl);
+        logger.debug("Saved entry");
+    }
 }
